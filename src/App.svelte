6823aa38e247e4cd49ba8c322d83c286d14e887b
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-31 12:34:24 +0000

    updated

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,11 @@
 	import {Router, Route} from 'svelte-routing';
 	import Heroes from './heroes/pages/Heroes.svelte';
 	import HeroDetail from './heroes/pages/HeroDetail.svelte';
+	import NavBar from './shared/components/NavBar.svelte';
 </script>
 
 <Router url="" >
-	<!-- TODO: NavBar -->
+	<NavBar />
 	<Route path="/" component={Heroes} />
 	<Route path="hero-detail/:id" let:params >
 		<HeroDetail id="{params.id}" />
--- src/heroes/pages/Heroes.svelte
+++ src/heroes/pages/Heroes.svelte
@@ -1,1 +1,17 @@
+<script>
+    import {get} from '../../shared/api.call.js';
+    import {onMount} from 'svelte';
+    let heroes = [];
+    onMount(async () => {
+       const {data} = await get('heroes')
+       heroes = data;
+    })
+
+</script>
 <h2>Heroes Works!</h2>
+<div>
+    {#each heroes as hero}
+        <h2 key={hero.id}>{hero.firstName}</h2>
+    {/each}
+</div>
+
