701b59bf5e34bfa01751ca063678b9cdf9a9ea2c
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-31 12:14:43 +0000

    updated

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,13 @@
 <script>
-	export let name;
+	import {Router, Route} from 'svelte-routing';
+	import Heroes from './heroes/pages/Heroes.svelte';
+	import HeroDetail from './heroes/pages/HeroDetail.svelte';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Router url="" >
+	<!-- TODO: NavBar -->
+	<Route path="/" component={Heroes} />
+	<Route path="hero-detail/:id" let:params >
+		<HeroDetail id="{params.id}" />
+	</Route>
+</Router>
---
+++ src/heroes/pages/HeroDetail.svelte
@@ -0,0 +1,1 @@
+<h2>HeroDetail Works!</h2>
