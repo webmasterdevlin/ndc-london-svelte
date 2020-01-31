73a298147a2ff63f47a9651f21e1d29714cc140b
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-31 12:26:05 +0000

    updated

---
+++ src/shared/components/NavBar.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {Link} from 'svelte-routing';
+</script>
+
+<nav>
+    <Link to="/">
+        SVELTE APP
+    </Link>
+    <Link to="/">
+        Heroes
+    </Link>
+</nav>
+
+<style>
+  .logo {
+    width: 10rem;
+    height: auto;
+    margin: 1rem 2rem 1rem 1rem;
+  }
+  .nav-divider {
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+  .nav {
+    display: flex;
+    align-items: center;
+  }
+</style>
