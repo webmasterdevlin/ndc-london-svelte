5e9a8382a761d9287e73abcafbc987eb9fcd0be4
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-31 13:54:35 +0000

    updated

--- src/heroes/pages/Heroes.svelte
+++ src/heroes/pages/Heroes.svelte
@@ -1,17 +1,25 @@
 <script>
+    import { heroStore } from '../hero.store.js'
     import {get} from '../../shared/api.call.js';
     import {onMount} from 'svelte';
     let heroes = [];
+
     onMount(async () => {
-       const {data} = await get('heroes')
-       heroes = data;
+       await heroStore.loadHeroes();
     })
 
 </script>
 <h2>Heroes Works!</h2>
+
 <div>
-    {#each heroes as hero}
-        <h2 key={hero.id}>{hero.firstName}</h2>
-    {/each}
+    {#if $heroStore.isLoading}
+        <h2>Loading.. Please wait..</h2>
+    {:else}
+        {#each $heroStore.heroes as hero}
+            <li key={hero.id}>
+                {hero.firstName} {hero.lastName}
+            </li>
+        {/each}
+    {/if}
 </div>
 
